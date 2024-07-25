8b1d6622ede575bab9f3f39b26e4508f90f38576
SurabhiBorase <surabhiborase44@gmail.com>
2024-07-25 19:25:07 +0400

    [ADDED]: Added Factorial Number

--- main.py
+++ main.py
@@ -15,9 +15,3 @@ def sum_of_list(numbers):
 # Get user input for factorial calculation
 number = int(input("Enter a number to calculate its factorial: "))
 print(f"The factorial of {number} is {factorial(number)}")
-
-#Get user input for list sum calculation
-print("**** Function that calculates the sum of a list of numbers ****")
-
-list_of_numbers = list(map(int, input("Enter a list of numbers separated by spaces to calculate their sum: ").split()))
-print(f"The sum of the list {list_of_numbers} is {sum_of_list(list_of_numbers)}")
